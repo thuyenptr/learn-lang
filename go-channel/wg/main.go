f96a7ff6429f8e1341c876274f0dcee45331ea2e
Meteor <phantrongthuyen9d@gmail.com>
2019-09-16 14:11:23 +0700

    handle multiple request

--- go-channel/wg/main.go
+++ go-channel/wg/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"github.com/sirupsen/logrus"
 	"sync"
-	"time"
 )
 
 /**
@@ -26,44 +26,79 @@ type Request struct {
 	wg    sync.WaitGroup
 }
 
-func (h *Handler) convertValueToRequest(value int) error {
+func (h *Handler) convertValueToRequest(wg *sync.WaitGroup, value int) error {
 	rq := &Request{
 		value: value,
 	}
 
-	fmt.Printf("convert value %d\n", value)
+	logrus.Infof("convert value %d\n", value)
 	rq.wg.Add(1)
 	h.requestChan <- rq
 	rq.wg.Wait()
+	wg.Done()
 	return nil
 }
 
+func (h *Handler) displayChannelSize(ctx context.Context) {
+	go func() {
+		for {
+			logrus.Infof("[Size] Channel size %d", len(h.requestChan))
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
 func releaseRequest(batch []*Request) {
-	fmt.Println("release request")
 	for _, req := range batch {
+		logrus.Infof("release request value %d", req.value)
 		req.wg.Done()
 	}
 }
 
-func (h *Handler) consumeRequest(wg sync.WaitGroup) {
+func consumeRequest(ctx context.Context, reqs chan *Request) {
 	var count int
 	var bufReqs []*Request
 
 	for {
 		select {
-		case req, _ := <-h.requestChan:
+		case req, ok := <-reqs:
+			logrus.Infof("case receive request from channel")
+			if !ok {
+				return
+			}
+
 			count++
 			bufReqs = append(bufReqs, req)
-			if count == 2 {
+			if count > 2 {
+				logrus.Info("count > 2")
 				releaseRequest(bufReqs)
 				count = 0
 				bufReqs = bufReqs[:0]
 			}
-			if count == 4 {
-				wg.Done()
-				//return
+			logrus.Infof("request value %d\n", req.value)
+		case <-ctx.Done():
+			return
+		default:
+			logrus.Info("case default")
+			if len(bufReqs) > 0 {
+				releaseRequest(bufReqs)
+				count = 0
+				bufReqs = bufReqs[:0]
+				continue
 			}
-			fmt.Printf("request value %d\n", req.value)
+
+			req, ok := <-reqs
+			if !ok {
+				return
+			}
+			bufReqs = append(bufReqs, req)
+			count++
 		}
 	}
 }
@@ -71,13 +106,18 @@ func (h *Handler) consumeRequest(wg sync.WaitGroup) {
 func main() {
 	var wg sync.WaitGroup
 	handler := NewHandler()
-	wg.Add(1)
-	go handler.consumeRequest(wg)
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go consumeRequest(ctx, handler.requestChan)
+	go handler.displayChannelSize(ctx)
 
-	for i := 1; i < 5; i++ {
-		_ = handler.convertValueToRequest(i)
+	for i := 1; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			_ = handler.convertValueToRequest(&wg, i)
+		}()
 	}
 
-	time.Sleep(1 * time.Second)
 	wg.Wait()
+	cancelFunc()
+	close(handler.requestChan)
 }
